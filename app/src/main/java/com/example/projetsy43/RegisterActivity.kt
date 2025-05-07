ba91f1b0da4fe005a7039bd001be9d7a11e8f950
fisha <nurhafisha15@gmail.com>
2025-05-07 10:19:15 +0200

    Add Bottom Nav bar

--- app/src/main/java/com/example/projetsy43/RegisterActivity.kt
+++ app/src/main/java/com/example/projetsy43/RegisterActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 class RegisterActivity : ComponentActivity() {
@@ -27,4 +29,10 @@ class RegisterActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPreview() {
+    RegisterActivity()
 }
