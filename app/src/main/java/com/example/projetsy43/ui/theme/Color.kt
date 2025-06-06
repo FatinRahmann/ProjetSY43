e5b9ee5a6c7687a076892c9bb545ed9874d39fc8
fisha <nurhafisha15@gmail.com>
2025-06-06 23:59:31 +0200

    Change Theme.kt to use in LoginScreen.kt & RegisterScreen.kt

--- app/src/main/java/com/example/projetsy43/ui/theme/Color.kt
+++ app/src/main/java/com/example/projetsy43/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.projetsy43.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+// Light Theme Colors
+val LightGray = Color(0xFFF5F5F5)  // Background
+val White = Color(0xFFFFFFFF)      // Surface/Cards
+val DarkGrayText = Color(0xFF333333) // Text/icons
+
+// Dark Theme Colors
+val DarkGray = Color(0xFF121212)   // Background
+val Black = Color(0xFF000000)      // Surface/Cards
+val LightGrayText = Color(0xFFE0E0E0) // Text/icons
+
+// Shared Colors (Error, etc.)
+val ErrorRed = Color(0xFFCF6679)
