5ae91f939552e1e9634bc67253dd410061ebc988
voisin <voisinflorent4@gmail.com>
2025-06-24 14:45:03 +0200

    add filter on address

--- app/src/main/java/com/example/projetsy43/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/projetsy43/viewmodel/HomeViewModel.kt
@@ -18,9 +18,7 @@ class HomeViewModel(
     //Filtering events
     val filteredEvents: List<Event>
         get() = allEvents.filter {
-            it.name.contains(searchQuery, ignoreCase = true) ||
-            //TODO: Add more filters
-            it.attraction.contains(searchQuery, ignoreCase = true)
+            it.name.contains(searchQuery, ignoreCase = true) || it.attraction.contains(searchQuery, ignoreCase = true) || it.address.contains(searchQuery, ignoreCase = true)
         }
 
     //For the event selection
